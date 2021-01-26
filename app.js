414598447a96a26926cf8f40f0d8c2dfbff2e111
Jacob Stiles <jacobstiles@Jacobs-MacBook-Pro.local>
2021-01-26 13:37:32 -0500

    score on page

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ let hot100url = `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/w
 
 let snippeturl = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.snippet.get?track_id=`
 
+let score = 0
+let questionTracker = 0
+let scoreContainer = document.querySelector('#score')
+scoreContainer.innerHTML = `Score: ${score}/${questionTracker}`
+
 // Get an array of the hot 100 tracks and produce and array of 3 random tracks from it
 
 const getHot100 = async () => {
@@ -15,20 +20,21 @@ const getHot100 = async () => {
     let hot100Random1 = hot100Tracks[Math.floor((Math.random() * 100) + 1)]
     // console.log(hot100Random1)
     let hot100Random2 = hot100Tracks[Math.floor((Math.random() * 100) + 1)]
-      if (hot100Random1 === hot100Random2) {
+      if (hot100Random1.track.artist_name === hot100Random2.track.artist_name) {
         let hot100Random2 = hot100Tracks[Math.floor((Math.random() * 100) + 1)]
       }
     //  console.log(hot100Random2)
     let hot100Random3 = hot100Tracks[Math.floor((Math.random() * 100) + 1)]
-    if (hot100Random3 === hot100Random2 || hot100Random3 === hot100Random2) {
+    if (hot100Random3.track.artist_name === hot100Random2.track.artist_name || hot100Random3.track.artist_name === hot100Random2.track.artist_name) {
         let hot100Random3 = hot100Tracks[Math.floor((Math.random() * 100) + 1)]
       }
-     console.log(hot100Random3)
+    //  console.log(hot100Random3)
     let hot100RandomArray = [hot100Random1, hot100Random2, hot100Random3]
     // console.log(hot100RandomArray)
     // console.log(hot100RandomArray[Math.floor((Math.random() * 2) + 0)].track.track_id)
 
     getLyricSnippet(hot100RandomArray)
+    
 
     
 
@@ -42,9 +48,14 @@ getHot100()
 // retrieve lyric snippet from 1 of the tracks from hot100RandomArray
 
 const getLyricSnippet = async (arr) => {
+  
+  
   try {
-    let lyricResponse = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.snippet.get?track_id=${arr[Math.floor((Math.random() * 2) + 0)].track.track_id}&apikey=6a33c1748a705c73d54b40d8deb5038a`)
-    //  console.log(lyricResponse)
+    let snippetTrackID = arr[Math.floor((Math.random() * 2) + 0)].track.track_id
+    // console.log(snippetTrackID)
+
+    let lyricResponse = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.snippet.get?track_id=${snippetTrackID}&apikey=6a33c1748a705c73d54b40d8deb5038a`)
+    // console.log(lyricResponse)
 
     let lyricSnippet = lyricResponse.data.message.body.snippet.snippet_body
     // console.log(lyricSnippet)
@@ -52,8 +63,89 @@ const getLyricSnippet = async (arr) => {
     // console.log(snippetContainer)
     snippetContainer.innerHTML = lyricSnippet
 
+    let choice1Container = document.querySelector('#choice-one')
+    choice1Container.innerHTML = arr[0].track.artist_name
+    choice1Container.addEventListener('click', () => {
+      if (snippetTrackID === arr[0].track.track_id) {
+        console.log('You Are Correct')
+        score++
+        questionTracker++
+        choice1Container.disabled = true;
+        choice2Container.disabled = true;
+        choice3Container.disabled = true;
+        
+
+      } else {
+        console.log('Incorrect')
+        questionTracker++
+        choice1Container.disabled = true;
+        choice2Container.disabled = true;
+        choice3Container.disabled = true;
+        
+       
+      }
+      let scoreContainer = document.querySelector('#score')
+      scoreContainer.innerHTML = `Score: ${score}/${questionTracker}`
+      
+    })
+
+
+
+    let choice2Container = document.querySelector('#choice-two')
+    choice2Container.innerHTML = arr[1].track.artist_name
+    choice2Container.addEventListener('click', () => {
+      if (snippetTrackID === arr[1].track.track_id) {
+        console.log('You Are Correct')
+        score++
+        questionTracker++
+        choice1Container.disabled = true;
+        choice2Container.disabled = true;
+        choice3Container.disabled = true;
+        
+
+      } else {
+        console.log('Incorrect')
+        questionTracker++
+        choice1Container.disabled = true;
+        choice2Container.disabled = true;
+        choice3Container.disabled = true;
+        
+
+      }
+      let scoreContainer = document.querySelector('#score')
+      scoreContainer.innerHTML = `Score: ${score}/${questionTracker}`
+      
+    })
+
+    let choice3Container = document.querySelector('#choice-three')
+    choice3Container.innerHTML = arr[2].track.artist_name
+    choice3Container.addEventListener('click', () => {
+      if (snippetTrackID === arr[2].track.track_id) {
+        console.log('You Are Correct')
+        score++
+        questionTracker++
+        choice1Container.disabled = true;
+        choice2Container.disabled = true;
+        choice3Container.disabled = true;
+        
+
+      } else {
+        console.log('Incorrect')
+        questionTracker++
+        choice1Container.disabled = true;
+        choice2Container.disabled = true;
+        choice3Container.disabled = true;
+        
+
+      }
+      let scoreContainer = document.querySelector('#score')
+      scoreContainer.innerHTML = `Score: ${score}/${questionTracker}`
+
+    })
+
   } catch (error) {
     console.log(error)
   }
   
 }
+
